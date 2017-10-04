87c836194f19dc303c2a34cf2e5fc75bc6ad7c4d
James M. Joyce <james@flashpointcs.net>
2017-10-03 20:02:02 -0500

    Initial commit

---
+++ qr/index.php
@@ -0,0 +1,14 @@
+<?php
+header("Location: http://www.flashpointcs.net");
+?>
+<!DOCTYPE html> 
+<html> 
+  <head> 
+    <title>FPCS</title> 
+    <META http-equiv="refresh" content="1;URL=http://www.flashpointcs.net"> 
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  </head> 
+  <body> 
+		If you are not redirected, please <a href="http://www.flashpointcs.net">click here</a>.
+  </body> 
+</html>
